9c64872a8de0b67725c4fa99b72e4d355b488c4b
Nerina Bonnin <nerinabonnin07@gmail.com>
2026-07-21 20:33:18 -0300

    tratando de que ande el backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-});
-
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+  baseURL: process.env.REACT_APP_API_URL
+    ? `${process.env.REACT_APP_API_URL}/api`
+    : 'http://localhost:5000/api',
 });
 
 export default api;
