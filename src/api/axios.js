2eee345ae74f3105f8037cd415f97b1859b2a736
Nerina Bonnin <nerinabonnin07@gmail.com>
2026-07-21 20:48:57 -0300

    intentando devuelta resolver el backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// Vite lee las variables con import.meta.env.VITE_...
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
     ? `${import.meta.env.VITE_API_URL}/api`
